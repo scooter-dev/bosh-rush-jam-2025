18f946aee283c648b8fc77106881390ed2a5a6cc
Scooter <dogoneitdogs@yahoo.com>
2025-02-02 04:34:04 -0500

    ui fix

--- SRC/UI/ZombieMenus/inventory_margin.gd
+++ SRC/UI/ZombieMenus/inventory_margin.gd
@@ -22,48 +22,55 @@ var inventoryVisible: bool = false
 var fadeCounter: float = 0.00;
 
 func _ready():
-    # signal from player when inv update to change count on hud
-    PlayerManager.updateHudCount.connect(changeCount)
+	# signal from player when inv update to change count on hud
+	PlayerManager.updateHudCount.connect(changeCount)
 
 
-func changeCount(newCount: int, itemName: PlayerManager.e_items)-> void:
-    var currentLabel: Label
-    match itemName:
-        e_items.GLUE:
-            currentLabel = glueCount
-        e_items.STAPLES:
-            currentLabel = stapleCount
-        e_items.PHONE_BOOK:
-            currentLabel = phoneBookCount
-        e_items.SODA:
-            currentLabel = sodaCount
-        e_items.CANDY:
-            currentLabel = candyCount
-        e_items.TUBING:
-            currentLabel = tubingCount
-        e_items.CHIP:
-            currentLabel = chipCount
-        e_items.CIRCUIT_BOARD:
-            currentLabel = circuitboardCount
-        e_items.OIL:
-             currentLabel = oilCount
+func changeCount(newCount: int, itemName: PlayerManager.e_items) -> void:
+	var currentLabel: Label
+	match itemName:
+		e_items.GLUE:
+			currentLabel = glueCount
+		e_items.STAPLES:
+			currentLabel = stapleCount
+		e_items.PHONE_BOOK:
+			currentLabel = phoneBookCount
+		e_items.SODA:
+			currentLabel = sodaCount
+		e_items.CANDY:
+			currentLabel = candyCount
+		e_items.TUBING:
+			currentLabel = tubingCount
+		e_items.CHIP:
+			currentLabel = chipCount
+		e_items.CIRCUIT_BOARD:
+			currentLabel = circuitboardCount
+		e_items.OIL:
+			currentLabel = oilCount
+		e_items.PEN:
+			currentLabel = penCount
+		e_items.WIRES:
+			currentLabel = wiresCount
+		e_items.TAPE:
+			currentLabel = tapeCount
+	currentLabel.set_text(String.num(newCount))
+	print_debug("yum, a"+ PlayerManager.getItemName(itemName)+ ". Now you have "+String.num(newCount))
+	if newCount > 0:
+		currentLabel.show()
+	else:
+		currentLabel.hide()
 
-    currentLabel.set_text(String.num(newCount))
-    if newCount > 0:
-        currentLabel.show()
-    else:
-        currentLabel.hide()
 
-
-func showInventory()-> void:
-    inventoryVisible = true
-    inventoryGrid.show()
-    fadeCounter = 1.00
+func showInventory() -> void:
+	inventoryVisible = true
+	inventoryGrid.show()
+	fadeCounter = 1.00
 
 func _process(delta):
-    if inventoryVisible == true:
-        fadeCounter-=delta
+	pass
+	# if inventoryVisible == true:
+	# 	fadeCounter -= delta
 
-    if fadeCounter<= 0:
-        inventoryGrid.hide()
-        inventoryVisible = false
+	# if fadeCounter <= 0:
+	# 	inventoryGrid.hide()
+	# 	inventoryVisible = false
